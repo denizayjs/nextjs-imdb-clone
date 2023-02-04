12c5a184c89474b077693bae1703c95838cc64e1
denizayjs <denizay@std.iyte.edu.tr>
2023-02-04 15:41:28 +0300

    create header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,17 @@
 import "./globals.css";
-
+import Header from "@/components/Header";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+        <Header />
+        {/* Navbar */}
+
+        {/* SearchBox */}
+
+        {children}
+      </body>
     </html>
   );
 }
