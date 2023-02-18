d336b857b6c5804d0c19bc52f763b5070eb52364
denizayjs <denizay@std.iyte.edu.tr>
2023-02-18 09:42:59 +0300

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,19 @@
 import "./globals.css";
 import Header from "@/components/Header";
+import Providers from "./Providers";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* Header */}
-        <Header />
-        {/* Navbar */}
+        <Providers>
+          {/* Header */}
+          <Header />
+          {/* Navbar */}
 
-        {/* SearchBox */}
+          {/* SearchBox */}
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
