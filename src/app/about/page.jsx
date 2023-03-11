ba55355dfafff78dbb8edb17e02f5c6792391e4c
denizayjs <denizay@std.iyte.edu.tr>
2023-03-11 13:11:56 +0300

    feat: create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatibus
+        modi accusamus. Aspernatur hic tempora perferendis nulla quia quaerat.
+        Suscipit aliquam dolor architecto cum, odit maiores quis id itaque ex
+        animi reprehenderit a alias, voluptate quasi beatae harum dolorem quod
+        ipsam similique. Voluptatibus laboriosam itaque quisquam vitae
+        voluptates ipsa perferendis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatibus
+        modi accusamus. Aspernatur hic tempora perferendis nulla quia quaerat.
+        Suscipit aliquam dolor architecto cum, odit maiores quis id itaque ex
+        animi reprehenderit a alias, voluptate quasi beatae harum dolorem quod
+        ipsam similique. Voluptatibus laboriosam itaque quisquam vitae
+        voluptates ipsa perferendis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatibus
+        modi accusamus. Aspernatur hic tempora perferendis nulla quia quaerat.
+        Suscipit aliquam dolor architecto cum, odit maiores quis id itaque ex
+        animi reprehenderit a alias, voluptate quasi beatae harum dolorem quod
+        ipsam similique. Voluptatibus laboriosam itaque quisquam vitae
+        voluptates ipsa perferendis?
+      </p>
+    </div>
+  );
+}
