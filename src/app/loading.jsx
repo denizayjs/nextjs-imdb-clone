f3e1c757bba4bba44afaacfaf4d8dc125773de1e
denizayjs <denizay@std.iyte.edu.tr>
2023-03-26 10:50:25 +0300

    feat: add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center">
+      <img className="h-76" src="loading.svg" alt="loading" />
+    </div>
+  );
+}
