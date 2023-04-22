33ad67133b762c9ba1a3f6a9afe4ee1004353a12
denizayjs <denizay@std.iyte.edu.tr>
2023-04-22 13:12:41 +0300

    fix: component doesnt resolve

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import Providers from "./Providers";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
